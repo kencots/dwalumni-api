5f2db250cf60309cb91c772456a1ad8a292b8afa
Dicky Indra Asmara <dickyindraofficial@gmail.com>
2018-06-27 03:08:24 +0700

    adding detail_skill module

---
+++ detail_skill/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+from django.contrib.auth.models import User
+from skill.models import Skill
+
+class DetailSkill(models.Model):
+    class Meta:
+        db_table = "detail_skill"
+
+    name = models.CharField(max_length=50)
+    skill = models.ForeignKey(
+        Skill,
+        on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='detail_skill_created_by'
+    )
+    updated_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='detail_skill_updated_by'
+    )
+
+class UserDetailSkill(models.Model):
+    class Meta:
+        db_table = "detail_user_skill"
+
+    skill_exam_detail = models.ForeignKey(
+        DetailSkill,
+        on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='detail_user_skill_created_by'
+    )
+    updated_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='detail_user_skill_updated_by'
+    )
